90a44ba5ebf5b3b61b62ebf85276721434af7e87
Andreu <andreu.country.dev@gmail.com>
2026-02-05 15:30:17 +0100

    apply new feature to our model of rolecito gameplay

--- rolecito.cc
+++ rolecito.cc
@@ -11,13 +11,27 @@ struct BasicInfo {
     char culture[30];
 };
 
+const int numPoolValues = 6;
+
 struct CharacterPool {
-    int constitution, strenght, charism, inteligence, knowledge, faith;
+    int constitution, strenght, charism, intelligence, knowledge, faith;
 };
 
 struct Skills {
     int talkative, combat, hide, stealth, health, fatigue, jump, mana;
 };
+
+struct Character {
+    BasicInfo basicInfo;
+    CharacterPool pool;
+    Skills skills;
+};
+
+Character character;
+
+void GenerateSeed() {
+    srand(time(NULL));
+}
 
 void PrepareFile() {
     f = fopen("files/model.dat", "r+b");
@@ -29,22 +43,122 @@ void PrepareFile() {
 
 void AskBasic() {
 
+    printf("Name of your character: ");
+    fgets(character.basicInfo.name, 15, stdin);
+
+    printf("Short description of your character: ");
+    fgets(character.basicInfo.description, 100, stdin);
+
+    printf("Associated culture (Example: medieval knight, native refugee): ");
+    fgets(character.basicInfo.culture, 30, stdin);
+}
+
+void CalculatePoolsFields(int pool, int num1, int num2, int num3) {
+
+    switch (pool) {
+        case 0:
+            character.pool.constitution = num1 + num2 + num3;
+        break;
+        case 1:
+            character.pool.strenght = num1 + num2 + num3;
+        break;
+        case 2:
+            character.pool.charism = num1 + num2 + num3;
+        break;
+        case 3:
+            character.pool.intelligence = num1 + num2 + num3;
+        break;
+        case 4:
+            character.pool.knowledge = num1 + num2 + num3;
+        break;
+        case 5:
+            character.pool.faith = num1 + num2 + num3;
+        break;
+    }
+}
+
+void printBasicInfo() {
+    printf("Here it is your normal information: \n");
+    printf("Name: %s", character.basicInfo.name);
+    printf("Description: %s", character.basicInfo.description);
+    printf("Culture: %s", character.basicInfo.culture);
+    printf("\n");
+}
+
+void printPool() {
+    printf("Here it is your pool of atributes: \n");
+    printf("Constitution: %d \n", character.pool.constitution);
+    printf("Strenght: %d \n", character.pool.strenght);
+    printf("Charism: %d \n", character.pool.charism);
+    printf("Intelligence: %d \n", character.pool.intelligence);
+    printf("Knowledge: %d \n", character.pool.knowledge);
+    printf("Faith: %d \n \n", character.pool.faith);
 }
 
 void AskCharacterPool() {
+    GenerateSeed();
 
+    printf("We are gonna use 'dados' to calcule your values... \n");
+
+    for (int i = 0; i < numPoolValues; i++) {
+        int num1 = rand()%5 + 1, num2 = rand()%5 + 1, num3 = rand()%5 + 1;
+
+        CalculatePoolsFields(i, num1, num2, num3);
+    }
+    char response;
+    printf("End, do you want to see it (Y/n)?");
+    scanf("%c", &response);
+    printf("\n");
+
+    if (response == 'Y') {
+        printPool();
+    }
 }
 
 void AskSkills() {
 
 }
 
-bool ValidateOption(int option) {
-    if (option < 1 || option > 2) {
-        return false;
-    }
+void WhatSee() {
+    
+    int option;
 
-    return true;
+    printf("1 -- See basic information \n");
+    printf("2 -- Another think \n");
+    printf("Option? ");
+    scanf("%d \n \n", &option);
+
+    switch (option) {
+        case 1:
+            printBasicInfo();
+        break;
+        case 2:
+            printPool();
+        break;
+    }
+}
+
+void AskModifiers() {
+
+}
+
+void DeleteCharacter() {
+
+}
+
+void ValidateOption(int option) {
+    
+    switch (option) {
+        case 1:
+            WhatSee();
+        break;
+        case 2:
+            AskModifiers();
+        break;
+        case 3:
+            DeleteCharacter();
+        break;
+    }
 }
 
 void AskWhatToDo() {
@@ -54,13 +168,14 @@ void AskWhatToDo() {
     printf("We have a preliminar version, what do you want to do? \n");
     printf("1 -- Check model \n");
     printf("2 -- Modify information \n");
-    printf("Option 1 or 2? ");
+    printf("3 -- Delete character \n");
+    printf("4 -- Exit... \n");
+    printf("Option ? ");
     scanf("%d", &option);
 
-    if (!ValidateOption(option)) {
-        printf("\n \n");
-        printf("This option doesn't exist.");
-        printf("\n");
+    ValidateOption(option);
+
+    if (option >= 1 && option <= 3) {
 
         AskWhatToDo();
     }
