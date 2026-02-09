44072bee81d88b91f4b8dfbeb825492463d65684
sanchezcas <sanchezcas@esatvalencia.lan>
2026-02-09 20:07:03 +0100

    implement all memory to prepare our draw settings

--- gameLife.cc
+++ gameLife.cc
@@ -10,15 +10,78 @@
 
 unsigned char si = 0, fps=25; //Control de frames por segundo
 double current_time, last_time;
-int windowX = 800, windowY = 800, numberFilas;
+int windowX = 800, windowY = 800, filas;
+char* born = nullptr;
+char* dead = nullptr;
 
-struct Celula {
-	esat::Vec2 coordenate;
-	bool IsAlive;
+struct Color {
+	int R, G, B;
 };
+
+struct TCelula {
+	esat::Vec2 initialPosition;
+	esat::Vec2 lastPosition;
+	bool IsAlive = false;
+	Color color;
+};
+
+TCelula* celulaData = nullptr;
 
 void GenerateSeed() {
     srand(time(NULL));
+}
+
+void TakeBornDead(char* argv) {
+
+	born = (char*) malloc((3)*sizeof(char));
+	dead = (char*) malloc((3)*sizeof(char));
+
+	bool isDead = false;
+
+	for (int i = 0; i < 7; i++) {
+
+		if (*(argv+i) != '/' && !isDead) {
+			*(born+i) = *(argv+i);
+		} else {
+			isDead = true;
+		}
+
+		if (isDead && i < 6) {
+			int j = 0;
+			*(dead+j) = *(argv+i+1);
+			j++;
+		}
+	}
+}
+
+void InitMemory() {
+	filas = filas * filas;
+	celulaData = (TCelula*) malloc(filas*sizeof(TCelula));
+}
+
+void InitMap() {
+	float coordenateX = 0.0f;
+	float coordenateY = windowY / filas;
+	for (int i = 0; i < filas; i++) {
+		int random = rand()%1;
+
+		if (random == 1) {
+			(celulaData+i)->IsAlive = true;
+			(celulaData+i)->color = {255, 0, 0};
+		}
+
+		if (coordenateX + (windowX / filas) < 800) {
+			(celulaData+i)->initialPosition = {coordenateX, coordenateY};
+			(celulaData+i)->lastPosition = {coordenateX + (windowX / filas), coordenateY};
+			coordenateX += windowX / filas;
+			esat::
+		} else {
+			coordenateX = 0;
+			coordenateY += windowY / filas;
+		}
+	}
+
+
 }
 
 int esat::main(int argc, char **argv) {
@@ -26,9 +89,13 @@ int esat::main(int argc, char **argv) {
     GenerateSeed();
 	
 	if (argc == 3) {
-        windowX = atoi(*(argv+1));
-        numberFilas = atoi(*(argv+2));
+    	fgets(*(argv+1), 8, stdin);
+        filas = atoi(*(argv+2));
     }
+
+	TakeBornDead(*(argv+1));
+	InitMemory();
+	InitMap();
 
 	esat::WindowInit(windowX, windowY);
 	WindowSetMouseVisibility(true);
