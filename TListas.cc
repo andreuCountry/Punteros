8cee1941f354983f9843f682424db2e0c0c4e40a
sanchezcas <sanchezcas@esatvalencia.lan>
2026-02-11 20:09:41 +0100

    add more function to pointers

--- TListas.cc
+++ TListas.cc
@@ -1,6 +1,81 @@
 #include <stdio.h>
 
+enum ACTUAL {
+    actuality,
+    anterior
+};
+
 struct TLista {
     int info;
     TLista *prox;
 };
+
+void CrearLista(TLista** lista) {
+    lista = nullptr;
+}
+// for(p=lista; p = nullptr; p = p->prox)
+
+void InsertarLista(TLista** lista, int valor) {
+    TLista* newLista = (TLista*) malloc(1*sizeof(TLista));
+    newLista->info = valor;
+    newLista->prox = *lista;
+    *lista = newLista;
+}
+
+void MuestraLista(TLista* lista) {
+
+    TLista* p;
+
+    for(p = lista; p != nullptr; p = p->prox) {
+        printf("La lista contiene valor %d \n", p->info);
+    }
+}
+
+TLista* IndexaLista(TLista* lista, int index) {
+    
+    TLista* p = lista;
+
+    for(int i = 0;  p != nullptr && i < index; p = p->prox) {
+        i++;
+    }
+
+    return p;
+}
+
+TLista* BuscarLista(TLista* lista, int index, ACTUAL direction) {
+    TLista* p = lista;
+    TLista* aux = nullptr;
+
+    for(int i = 0; p != nullptr && i != index; p = p->prox) {
+
+        if (direction == anterior) {
+            if (i + 1 == index) {
+                aux = p;
+            }
+        }
+
+        i++;
+    }
+
+    if (direction == anterior) {
+        return aux;
+    } else {
+        return p;
+    }
+}
+
+void EliminaEnLista(TLista** lista, int info) {
+    TLista* p = *lista;
+    TLista* aux = nullptr;
+    bool encontrado = false;
+
+    for(p = *lista;  p != nullptr && !encontrado; p = p->prox) {
+        aux = p->prox;
+        encontrado = aux->info == info;
+
+        if (encontrado) {
+            p = p->prox;
+            free(aux);
+        }
+    }
+}
--- listas.cc
+++ listas.cc
@@ -6,37 +6,7 @@
 
 TLista *lista1,*lista2;
 
-void CrearLista(TLista** lista) {
-    lista = nullptr;
-}
-// for(p=lista; p = nullptr; p = p->prox)
-
-void InsertarLista(TLista** lista, int valor) {
-    TLista* newLista = (TLista*) malloc(1*sizeof(TLista));
-    newLista->info = valor;
-    newLista->prox = *lista;
-    *lista = newLista;
-}
-
-void MuestraLista(TLista* lista) {
-
-    TLista* p;
-
-    for(p = lista; p != nullptr; p = p->prox) {
-        printf("La lista contiene valor %d \n", p->info);
-    }
-}
-
-TLista* IndexaLista(TLista* lista, int index) {
-    
-    TLista* p = lista;
-
-    for(int i = 0; i < index && p != nullptr; i++) {
-        p = lista->prox;
-    }
-
-    return p;
-}
+ACTUAL actual = actuality;
 
 int main() {
 
@@ -59,19 +29,17 @@ int main() {
     printf("\n\n Acceso indexado a lista por valor que ocupa posicion 0. \n");
     lista2=IndexaLista(lista1,0);
     printf(" Elemento apuntado de la lista: %d \n",lista2->info);
-    /*
     printf("\n\n Buscar y apuntar. \n");
     if((lista2=BuscarLista(lista1,3,actual))==nullptr){
         printf("\nNo encontrado.");
     } else {
         printf("\n Localizado y apuntado: %d",lista2->info);
     }
-
     printf("\n\n Localizar y eliminar. valores 28, 3 y 280\n");
     EliminaEnLista(&lista1,28);
     printf("\n Eliminado valor 28 de la lista\n");
     printf(" Lista >>> ");
-    MuestraLista(lista1);
+    MuestraLista(lista1);/*
     EliminaEnLista(&lista1,3);
     printf("\n Eliminado valor 3 de la lista\n");
     printf(" Lista >>> ");
