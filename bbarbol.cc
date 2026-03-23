7fd973e194d833cc14ecdb8006f0c1dff3cb64cc
sanchezcas <sanchezcas@esatvalencia.lan>
2026-03-23 20:24:29 +0100

    add code to bbarbol

--- bbarbol.cc
+++ bbarbol.cc
@@ -1,14 +1,54 @@
-struct Tbarbol {
+struct tbbarbol {
     int info;
-    Tbarbol *proxRight;
-    Tbarbol *proxLeft;
+    tbbarbol *proxRight;
+    tbbarbol *proxLeft;
 };
 
-Tbarbol crear_arbol() {
-    Tbarbol* barbol = (Tbarbol*) malloc(1*sizeof(Tbarbol));
+tbbarbol* crear_barbol() {
+    tbbarbol* barbol = (tbbarbol*) malloc(1*sizeof(tbbarbol));
 
     barbol->proxRight = nullptr;
     barbol->proxLeft = nullptr;
 
     return barbol;
+}
+
+void insertar_barbol(tbbarbol** barbol, int info) {
+    tbbarbol* newBarbol = (tbbarbol*) malloc(sizeof(tbbarbol));
+    newBarbol->proxRight = nullptr;
+    newBarbol->proxLeft = nullptr;
+
+    if ((*barbol)->info != info) {
+        newBarbol->info = info;
+        if ((*barbol)->info < info) {
+            (*barbol)->proxRight = newBarbol;
+        } else {
+            (*barbol)->proxLeft = newBarbol;
+        }
+    }
+}
+
+void MostrarArbol(tbbarbol* barbol, int desplazamiento) {
+
+    if (barbol != nullptr) {
+        // EHHHHHH, first para la derechita bro
+        if (barbol->proxRight != nullptr) {
+            MostrarArbol(barbol->proxRight, desplazamiento+1);
+        }
+
+        for (int i = 0; i < desplazamiento; i++) {
+            printf("   ");
+        }
+
+        // EHHHHHH, tenemos mid bro
+        printf("%03d \n", barbol->info);
+        
+        // EHHHHHH, acabamos a la izquierda bro
+        if (barbol->proxLeft != nullptr) {
+            MostrarArbol(barbol->proxLeft, desplazamiento+1);
+        }
+    } else {
+        printf("El arbolito no existe!!");
+    }
+    
 }
--- main_bbarbol.cc
+++ main_bbarbol.cc
@@ -37,13 +37,13 @@ int main(){
   MostrarArbol( barbol1, 0);
   printf("--------------------------------------------\n\n\n");
 
-  printf("In  : ");inorden(barbol1);printf("\n");
-  printf("Pre : ");preorden(barbol1);;printf("\n");
-  printf("Post: ");postorden(barbol1);;printf("\n");
+//   printf("In  : ");inorden(barbol1);printf("\n");
+//   printf("Pre : ");preorden(barbol1);;printf("\n");
+//   printf("Post: ");postorden(barbol1);;printf("\n");
   
-  buscado=buscar_barbol(barbol1,7);
-  if(buscado!=NULL) printf("\nEncontrado. Nodo apuntado %d",buscado->info);
-  else printf("\nNo encontrado");
+//   buscado=buscar_barbol(barbol1,7);
+//   if(buscado!=NULL) printf("\nEncontrado. Nodo apuntado %d",buscado->info);
+//   else printf("\nNo encontrado");
   
 
 
