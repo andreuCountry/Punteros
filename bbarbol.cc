e80aeaa1628cd4f79b73a6f461ef1b457ee1a275
sanchezcas <sanchezcas@esatvalencia.lan>
2026-03-24 20:06:28 +0100

    fix functions except search

--- bbarbol.cc
+++ bbarbol.cc
@@ -5,25 +5,50 @@ struct tbbarbol {
 };
 
 tbbarbol* crear_barbol() {
-    tbbarbol* barbol = (tbbarbol*) malloc(1*sizeof(tbbarbol));
-
-    barbol->proxRight = nullptr;
-    barbol->proxLeft = nullptr;
+    tbbarbol* barbol = nullptr;
 
     return barbol;
 }
 
-void insertar_barbol(tbbarbol** barbol, int info) {
-    tbbarbol* newBarbol = (tbbarbol*) malloc(sizeof(tbbarbol));
-    newBarbol->proxRight = nullptr;
-    newBarbol->proxLeft = nullptr;
+tbbarbol* buscar_barbol(tbbarbol* barbol, int info) {
+    if (barbol != nullptr) {
+        if (barbol->info != info) {
+            if (barbol->proxRight != nullptr) {
+                buscar_barbol(barbol->proxRight, info);
+            }
+            
+            if (barbol->proxLeft != nullptr) {
+                buscar_barbol(barbol->proxLeft, info);
+            }
 
-    if ((*barbol)->info != info) {
-        newBarbol->info = info;
-        if ((*barbol)->info < info) {
-            (*barbol)->proxRight = newBarbol;
+            return NULL;
         } else {
-            (*barbol)->proxLeft = newBarbol;
+            return barbol;
+        }
+    } else {
+        return NULL;
+    }
+}
+
+void insertar_barbol(tbbarbol** barbol, int info) {
+
+    if (buscar_barbol(*barbol, info) == NULL) {
+
+        // insertar
+        if ((*barbol) == nullptr) {
+            // guardar memoria en el tbbarol actual, que es insertable
+            *barbol = (tbbarbol*) malloc(sizeof(tbbarbol));
+
+            (*barbol)->proxRight = nullptr;
+            (*barbol)->proxLeft = nullptr;
+            
+            (*barbol)->info = info;
+        } else {
+            if ((*barbol)->info > info) {
+                insertar_barbol(&((*barbol)->proxLeft), info);
+            } else {
+                insertar_barbol(&((*barbol)->proxRight), info);
+            }
         }
     }
 }
@@ -51,4 +76,50 @@ void MostrarArbol(tbbarbol* barbol, int desplazamiento) {
         printf("El arbolito no existe!!");
     }
     
+}
+
+void inorden(tbbarbol* barbol) {
+    if (barbol != nullptr) {
+
+        if (barbol->proxLeft != nullptr) {
+            inorden(barbol->proxLeft);
+        }
+
+        printf("%d  ", barbol->info);
+
+        if (barbol->proxRight != nullptr) {
+            inorden(barbol->proxRight);
+        }
+    }
+}
+
+void preorden(tbbarbol* barbol) {
+    if (barbol != nullptr) {
+
+        printf("%d  ", barbol->info);
+
+        if (barbol->proxLeft != nullptr) {
+            preorden(barbol->proxLeft);
+        }
+
+        if (barbol->proxRight != nullptr) {
+            preorden(barbol->proxRight);
+        }
+    }
+}
+
+void postorden(tbbarbol* barbol) {
+    if (barbol != nullptr) {
+
+
+        if (barbol->proxLeft != nullptr) {
+            postorden(barbol->proxLeft);
+        }
+
+        if (barbol->proxRight != nullptr) {
+            postorden(barbol->proxRight);
+        }
+
+        printf("%d  ", barbol->info);
+    }
 }
